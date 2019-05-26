712461a0099bff29688e839921f5c05294a2e520
2quarius <863115889@qq.com>
2019-05-26 21:53:06 +0800

    security pro

--- demo/src/main/java/com/sixplus/demo/DemoApplication.java
+++ demo/src/main/java/com/sixplus/demo/DemoApplication.java
@@ -1,14 +1,11 @@
 package com.sixplus.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 @EnableJpaRepositories("com.sixplus.demo")
